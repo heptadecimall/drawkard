274e403589bfece4040b4f4a30c4e16131d8e945
lukman.ramli <lukman.ramli@infinitelogix.com.my>
2026-01-05 08:43:25 +0800

    fix routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
 import { Routes } from '@angular/router';
 import { App } from './app';
 
-export const routes: Routes = [
-    { path: '', redirectTo: '/drawkard', pathMatch: 'full' },
-    { path: 'drawkard', component: App },
-    { path: '**', redirectTo: '/drawkard' }
-];
+export const routes: Routes = [];
