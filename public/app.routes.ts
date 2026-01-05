28a99b8d4e69590403cd68c17946282a65786fed
lukman.ramli <lukman.ramli@infinitelogix.com.my>
2026-01-05 08:31:05 +0800

    fix the viewport screen on mobile screen. fix routing redirect

---
+++ public/app.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { App } from '../src/app/app';
+
+export const routes: Routes = [
+    { path: '', redirectTo: '/drawkard', pathMatch: 'full' },
+    { path: 'drawkard', component: App },
+    { path: '**', redirectTo: '/drawkard' }
+];
